09670cc6ec565aed949eeaa6bd0c9d9a216c840e
Fahim-F-Osman <fahimfosman@gmail.com>
2026-06-27 14:46:44 +1000

    feat(dark/light mode): implement theme logic using ThemeContext

--- app/src/components/UtilityMenu/UtilityMenu.tsx
+++ app/src/components/UtilityMenu/UtilityMenu.tsx
@@ -5,17 +5,18 @@ import {
   FaUserCircle,
 } from "react-icons/fa";
 import styles from "./UtilityMenu.module.css"
-import { useState } from "react";
+import { useTheme } from "@/context/theme/useTheme";
 
 export default function UtilityMenu() {
-  const [dark, setDark] = useState(false);
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <div className={styles.container}>
       <button
         className={styles.toggle}
-        onClick={() => setDark(!dark)}
+        onClick={toggleTheme}
       >
-        {dark ? <FaCloudMoonRain /> : <FaSun />}
+        {theme === "light" ? <FaSun /> : <FaCloudMoonRain />}
       </button>
       <button className={styles.contactsButton}><FaUserCircle /></button>
       <button><FaPalette /></button>
